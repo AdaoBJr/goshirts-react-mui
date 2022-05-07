1f5e203d7f7f5e23496a8338ac984733db0a7e2c
Adão Benites - ESX <esx48@localiza.com>
2022-05-06 23:07:27 -0400

    style: :lipstick: created inittial config Theme

---
+++ src/theme/index.ts
@@ -0,0 +1,77 @@
+import { createTheme } from '@mui/material';
+import { ptBR } from '@mui/material/locale';
+
+export const theme = createTheme(
+  {
+    spacing: 16,
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 641,
+        md: 1024,
+        lg: 1280,
+        xl: 1920,
+      },
+    },
+    palette: {
+      primary: {
+        main: '#dfe1e2',
+        dark: '#aabfb8',
+        light: '#e5e7e7',
+        contrastText: '#fff',
+      },
+      background: {
+        default: '#FAFAFA',
+        paper: '#fff',
+      },
+      error: {
+        main: '#FF2F2F',
+      },
+    },
+    typography: {
+      fontFamily: ['JetBrains', 'sans-serif'].join(','),
+      h6: {
+        fontSize: '1rem',
+        color: '#333333',
+        fontWeight: 700,
+        lineHeight: 1.5,
+      },
+      body1: {
+        color: '#424242',
+        fontWeight: 400,
+        fontSize: '1rem',
+      },
+      button: {
+        borderRadius: 6,
+      },
+    },
+    shadows: [
+      'none',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0px 35px 60px rgba(0, 0, 0, 0.25)',
+      '20px 55px 60px rgba(0, 0, 0, 0.25)',
+      '10px 15px 60px rgba(0, 0, 0, 0.25)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+      '0 2px 10px rgba(35, 0, 77, 0.2)',
+    ],
+  },
+  ptBR
+);
