5bc495f34e67b6d830b6743627e6d0210773ea48
Adão Benites - ESX <esx48@localiza.com>
2022-05-06 22:34:32 -0400

    feat: :tada: starting project

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './components/Home';
-import CreateAccount from './components/CreateAccount';
-
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/create-account" element={<CreateAccount />} />
+      <Route path="/" />
+      <Route path="/create-account" />
     </Routes>
   );
 };
