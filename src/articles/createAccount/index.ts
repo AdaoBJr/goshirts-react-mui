a1fa01b894da07e524190da6fe18dedafcf76eea
Adão Benites - ESX <esx48@localiza.com>
2022-05-07 17:13:14 -0400

    feat: :sparkles: created articles -> createAccount

---
+++ src/articles/createAccount/index.ts
@@ -0,0 +1,12 @@
+export const gender = [
+  { key: 1, label: 'Masculino', value: 0 },
+  { key: 2, label: 'Feminino', value: 1 }
+];
+
+export const country = [
+  { key: 1, label: 'Brasil', value: 'Brasil' },
+  { key: 2, label: 'Chile', value: 'Chile' },
+  { key: 3, label: 'Espanha', value: 'Espanha' },
+  { key: 4, label: 'Estados Unidos', value: 'Estados Unidos' },
+  { key: 5, label: 'Inglaterra', value: 'Inglaterra' }
+];
