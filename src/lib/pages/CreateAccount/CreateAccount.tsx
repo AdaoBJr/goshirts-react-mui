9508337090d8a7b9f46555b5bd9accedde1058d1
Adão Benites - ESX <esx48@localiza.com>
2022-05-08 19:00:21 -0400

    feat: :art: added TextInputs - firstname / lastname on CreateAccount

--- src/lib/pages/CreateAccount/CreateAccount.tsx
+++ src/lib/pages/CreateAccount/CreateAccount.tsx
@@ -1,38 +1,47 @@
 import React from 'react';
+import { Grid } from '@mui/material';
 
 import { country, gender } from '../../../articles';
-import { GridContainer, FormContainer, BoxWrapper, InputText } from './styles';
+import { GridContainer, FormContainer, PaperWrapper, InputText } from './styles';
 import { useCreateAccount } from '../../../services/talons';
 
 export const CreateAccount: React.FC = () => {
   const { handleSubmit, handleChange, values } = useCreateAccount();
 
   return (
-    <GridContainer container spacing={4} sx={{ margin: { xs: 'inherint', md: 'auto' } }}>
-      <FormContainer onSubmit={handleSubmit} autoComplete="off" noValidate>
-        <BoxWrapper>
-          <InputText
-            id="firstname"
-            name="firstname"
-            variant="outlined"
-            inputProps={{
-              'data-testid': 'firstname',
-              value: values.firstname,
-              onChange: handleChange
-            }}
-          />
-          <InputText
-            id="lastname"
-            name="lastname"
-            variant="outlined"
-            inputProps={{
-              'data-testid': 'lastname',
-              value: values.lastname,
-              onChange: handleChange
-            }}
-          />
-        </BoxWrapper>
-      </FormContainer>
+    <GridContainer>
+      <PaperWrapper>
+        <FormContainer onSubmit={handleSubmit} autoComplete="off" noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <InputText
+                id="firstname"
+                name="firstname"
+                variant="outlined"
+                fullWidth
+                inputProps={{
+                  'data-testid': 'firstname',
+                  value: values.firstname,
+                  onChange: handleChange
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <InputText
+                id="lastname"
+                name="lastname"
+                variant="outlined"
+                fullWidth
+                inputProps={{
+                  'data-testid': 'lastname',
+                  value: values.lastname,
+                  onChange: handleChange
+                }}
+              />
+            </Grid>
+          </Grid>
+        </FormContainer>
+      </PaperWrapper>
     </GridContainer>
   );
 };
